d36738f45cb6cfb39d34f62f85bf8f82eed4c57a
Dominik Giroux <giroux.dominik@gmail.com>
2021-05-24 22:15:33 -0400

    Updated layout, added 2.50, added some texts, changed some images and added a new one for success page

--- public/js/share.js
+++ public/js/share.js
@@ -2,8 +2,8 @@ function share() {
   if (navigator.share) {
     navigator
       .share({
-        title: "Save Nessie | Loch Ness Monster",
-        text: "I only need about tree fiddy... Damn Loch Ness Monster! This is a reference from South Park used in a context of a school art project.",
+        title: "Do you want to buy some cookies?",
+        text: "Girl Scout Cookies - Save Nessie",
         url: "https://save-nessie.web.app/"
       })
       .then(() => console.log("Thanks for sharing"))
---
+++ public/js/stripe.js
@@ -0,0 +1,39 @@
+(function () {
+  var stripe = Stripe(
+    "pk_live_51IuShoE8f20dSLXPCwEAJ3uEgtFxZ8aRVdKlaEHgVeeSWIRowmUZQj9lOKnZljevO7pZdfvjV59s15eKSdqh5Qr900nxbthQ7H"
+  );
+
+  var checkoutButton = document.getElementById(
+    "checkout-button-price_1IuSoYE8f20dSLXPAE8Lguaz"
+  );
+  checkoutButton.addEventListener("click", function () {
+    /*
+     * When the customer clicks on the button, redirect
+     * them to Checkout.
+     */
+    stripe
+      .redirectToCheckout({
+        lineItems: [{ price: "price_1IuSoYE8f20dSLXPAE8Lguaz", quantity: 1 }],
+        mode: "payment",
+        /*
+         * Do not rely on the redirect to the successUrl for fulfilling
+         * purchases, customers may not always reach the success_url after
+         * a successful payment.
+         * Instead use one of the strategies described in
+         * https://stripe.com/docs/payments/checkout/fulfill-orders
+         */
+        successUrl: "https://save-nessie.web.app/loch-ness-monster.html",
+        cancelUrl: "https://save-nessie.web.app/two-fiddy.html"
+      })
+      .then(function (result) {
+        if (result.error) {
+          /*
+           * If `redirectToCheckout` fails due to a browser or network
+           * error, display the localized error message to your customer.
+           */
+          var displayError = document.getElementById("error-message");
+          displayError.textContent = result.error.message;
+        }
+      });
+  });
+})();
